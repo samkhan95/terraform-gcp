16f42413812435207d038d35a7edc5d08e52d87c
samking7860 <samadkhansk5567@gmail.com>
2024-02-02 14:34:30 +0530

    uncomment the  code

--- lb.tf
+++ lb.tf
@@ -1,45 +1,45 @@
-# module "gce-lb-http" {
-#   source  = "terraform-google-modules/lb-http/google"
-#   version = "10.1.0"
-#   name    = var.network_prefix
-#   project = var.project_id
-#   target_tags = [
-#     "${var.network_prefix}-group1",
-#     "${var.network_prefix}-group2"
-#   ]
-#   firewall_networks = [module.vpc.network_name]
+module "gce-lb-http" {
+  source  = "terraform-google-modules/lb-http/google"
+  version = "10.1.0"
+  name    = var.network_prefix
+  project = var.project_id
+  target_tags = [
+    "${var.network_prefix}-group1",
+    "${var.network_prefix}-group2"
+  ]
+  firewall_networks = [module.vpc.network_name]
 
-#   backends = {
-#     default = {
+  backends = {
+    default = {
 
-#       protocol    = "HTTP"
-#       port        = 80
-#       port_name   = "http"
-#       timeout_sec = 10
-#       enable_cdn  = false
+      protocol    = "HTTP"
+      port        = 80
+      port_name   = "http"
+      timeout_sec = 10
+      enable_cdn  = false
 
-#       health_check = {
-#         request_path = "/"
-#         port         = 80
-#       }
+      health_check = {
+        request_path = "/"
+        port         = 80
+      }
 
-#       log_config = {
-#         enable      = true
-#         sample_rate = 1.0
-#       }
+      log_config = {
+        enable      = true
+        sample_rate = 1.0
+      }
 
-#       groups = [
-#         {
-#           group = module.vm_mig.instance_group
-#         },
-#         # {
-#         #   group = module.mig2.instance_group
-#         # },
-#       ]
+      groups = [
+        {
+          group = module.vm_mig.instance_group
+        },
+        # {
+        #   group = module.mig2.instance_group
+        # },
+      ]
 
-#       iap_config = {
-#         enable = false
-#       }
-#     }
-#   }
-# }
+      iap_config = {
+        enable = false
+      }
+    }
+  }
+}
